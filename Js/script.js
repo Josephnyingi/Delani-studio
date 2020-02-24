11e8c8e44d55df05d90ee151cdc9b3326b7ae927
Joseph Nyingi <josephnyingi@Users-MacBook-Pro.local>
2020-02-24 17:12:44 +0300

    add modified files styles,scripts.js and index.html

--- Js/script.js
+++ Js/script.js
@@ -1,21 +1,36 @@
-//    what we do section
+
+$("button").onClick(function () {
+    $("form").each(function () {
+      this.reset();
+    });
+    $(".card").hover(function () {
+      $("this").children(".card-1").fadeToggle(1000, "linear")
+    });
+  });
+
 $("#submit").click(function (event) {
   var name = $("#name").val();
   var email = $("#email").val();
   var message = $("#message").val();
   if (name === '' || email === '' || message === '') {
-      alert('Please fill in the required data');
+    alert('Please fill in the required data');
   } else {
-      alert(" Hello " + name + " We have received your message.Thank you for reaching out to us")
-      event.preventDefault();
+    alert(" Hello " + name + " We have received your message.Thank you for reaching out to us")
+    event.preventDefault();
   };
-  //    reset my form button
-  $("button").on('click', function () {
-    $('form').each(function () {
+
+
+  $("button").onClick(function () {
+    $("form").each(function () {
       this.reset();
     });
+    $(".card").hover(function () {
+      $("this").children(".card-1").fadeToggle(1000, "linear")
+    });
   });
-  //portifolio section 
-  $(".card").hover(function () {
-    $(this).children(".card-1").fadeToggle(1000, "linear");
-  });
+
+  function functionName() {
+    var delani=document.getElementById("web").value;
+    alert(delani+  " we have recorded your message. pleasure");
+
+}
